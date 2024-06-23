73338a26570e076af5de10a8ccd898a0eb4181fa
SebaGQ <segutierrez.q@gmail.com>
2024-06-23 00:01:15 -0400

    feat: se conecta front con back

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HttpService } from './services/http.service'; // Importar el servicio HTTP
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   displayText = false;
+  data: any;
+
+  constructor(private httpService: HttpService) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
 
   toggleText() {
     this.displayText = !this.displayText;
+  }
+
+  fetchData() {
+    this.httpService.get('avisos').subscribe(response => {
+      this.data = response;
+      console.log(this.data);
+    });
   }
 }
