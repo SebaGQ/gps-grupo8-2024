7d88c5f48ebda0084d9e7901ab73ac8873d5029a
SAVillena <143034116+SAVillena@users.noreply.github.com>
2024-07-19 00:18:20 -0400

    fix boton imagen + preview

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,36 +11,49 @@ import { BinnaclesComponent } from './components/binnacles/binnacles.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { AuthService } from './services/auth.service'; // Importar el servicio Auth
-import { HttpService } from './services/http.service'; // Importar el servicio HTTP
-import { OrderService } from './services/order.service'; // Importar el servicio Order
-import { OrderListComponent } from './components/order-list/order-list.component'; // Asegúrate de importar tu componente de órdenes
-import { MatSelectModule } from '@angular/material/select'; // Importa MatSelectModule
+import { AuthService } from './services/auth.service';
+import { HttpService } from './services/http.service';
+import { OrderService } from './services/order.service';
+import { OrderListComponent } from './components/order-list/order-list.component';
 
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-
-
-
-export function tokenGetter() {
-  return localStorage.getItem('token');
-}
-import { SpacesComponent } from './spaces/spaces.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SpaceFormComponent } from './space-form/space-form.component';  // Importar MatProgressSpinnerModule
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+import { SpacesComponent } from './spaces/spaces.component';
+import { SpaceFormComponent } from './space-form/space-form.component';
 import { BookingComponent } from './booking/booking.component';
 import { UserBookingsComponent } from './user-bookings/user-bookings.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD/MM/YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @NgModule({
   declarations: [
@@ -64,19 +77,17 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     JwtModule.forRoot({
       config: {
         tokenGetter,
-        allowedDomains: ['localhost:3000'], // Cambia esto a tu dominio permitido
-        disallowedRoutes: ['http://localhost:3000/auth/login'] // Cambia esto si es necesario
+        allowedDomains: ['localhost:3000'],
+        disallowedRoutes: ['http://localhost:3000/auth/login']
       }
     }),
     MatCardModule,
     MatProgressSpinnerModule,
-    BrowserAnimationsModule,  // Importar MatProgressSpinnerModule
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatCardModule,
-    ReactiveFormsModule,
     MatCheckboxModule,
     MatSelectModule,
     MatNativeDateModule,
@@ -86,7 +97,13 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatPaginatorModule,
     MatSortModule
   ],
-  providers: [AuthService, OrderService, HttpService],
+  providers: [
+    AuthService,
+    OrderService,
+    HttpService,
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
